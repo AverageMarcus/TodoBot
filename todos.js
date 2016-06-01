01a75b88ee4bc4640b73e9d2790f2c09470006d6
Marcus Noble <github@marcusnoble.co.uk>
2016-06-01 21:28:00 +0100

    Added boilerplate to todos

--- todos.js
+++ todos.js
@@ -1,3 +1,28 @@
-module.export = {
+"use strict";
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+const todoSchema = new Schema({
+  id: Number,
+  message: String
+});
+const userSchema = new Schema({
+  key: String,
+  todos: [todoSchema],
+  itemIndex: {type: Number, default: 0}
+});
+
+module.exports = {
+  addTodo: function(key, message) {
+
+  },
+  getTodos: function(key) {
+
+  },
+  completeTodo: function(key, todoId) {
+
+  },
+  removeTodo: function(key, todoId) {
+
+  }
 };
