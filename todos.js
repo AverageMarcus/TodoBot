15c454e128360834a9ef6d6a5c94dbbe2f427572
Marcus Noble <github@marcusnoble.co.uk>
2016-06-02 08:48:04 +0100

    Added minimum viable product todo storage

--- todos.js
+++ todos.js
@@ -12,17 +12,51 @@ const userSchema = new Schema({
   itemIndex: {type: Number, default: 0}
 });
 
+// MVP
+let db = {};
+
+function ensureKeyExists(key) {
+  if(!db.hasOwnProperty(key)) {
+    db[key] = {
+      itemIndex: 0,
+      todos: []
+    };
+  }
+}
+
 module.exports = {
   addTodo: function(key, message) {
+    ensureKeyExists(key);
 
+    let todoItem = {
+      id: ++db[key].itemIndex,
+      message: message
+    };
+
+    db[key].todos.push(todoItem);
+
+    return todoItem;
   },
   getTodos: function(key) {
+    ensureKeyExists(key);
 
+    let usersTodos = db[key].todos.filter(todo => todo.completed === undefined);
+
+    return usersTodos;
   },
   completeTodo: function(key, todoId) {
+    ensureKeyExists(key);
 
+    let todoItem = db[key].todos.find(todo => todo.id == todoId);
+    todoItem.completed = true;
+
+    return todoItem;
   },
   removeTodo: function(key, todoId) {
+    ensureKeyExists(key);
 
+    db[key].todos = db[key].todos.filter(todo => todo.id != todoId);
+
+    return this.getTodos(key);
   }
 };
